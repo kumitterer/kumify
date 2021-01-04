af5e2476802dc04f126cc0b60d3a34fcf063f673
Klaus-Uwe Mitterer <git@kumi.email>
2021-01-04 07:38:28 +0100

    Make activities not required for mood status

--- mood/forms.py
+++ mood/forms.py
@@ -6,7 +6,7 @@ from .models import Status, Activity
 
 class StatusForm(ModelForm):
     uploads = MultiFileField(required=False)
-    activities = ModelMultipleChoiceField(queryset=Activity.objects.all())
+    activities = ModelMultipleChoiceField(queryset=Activity.objects.all(), required=False)
 
     class Meta:
         model = Status
