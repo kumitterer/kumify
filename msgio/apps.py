6b1ff42f2f1ef4530f22491a79dec505a9449f0c
Kumi <git@kumi.email>
2021-02-20 14:17:52 +0000

    Fix matrix message delivery?

--- msgio/apps.py
+++ msgio/apps.py
@@ -1,7 +1,4 @@
 from django.apps import AppConfig
 
-import msgio.gateways.matrix
-import msgio.gateways.telegram
-
 class MsgioConfig(AppConfig):
     name = 'msgio'
--- msgio/views.py
+++ msgio/views.py
@@ -1,3 +1,6 @@
 from .handler import send_notifications
 
 from .gateways.telegram import TelegramWebhookView
+
+import msgio.gateways.matrix
+import msgio.gateways.telegram
